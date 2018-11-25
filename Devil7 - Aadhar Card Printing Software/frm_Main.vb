f5171231798b85b2af6e4337f0d52481b869b74e
Devil7DK <dineshthangavel47@gmail.com>
2018-11-25 20:33:48 +0530

    Implement Opening File from Command Line Args

--- Devil7 - Aadhar Card Printing Software/frm_Main.vb
+++ Devil7 - Aadhar Card Printing Software/frm_Main.vb
@@ -8,31 +8,9 @@ Public Class frm_Main
 
 #Region "Button Events"
     Private Sub btn_OpenPDF_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btn_OpenPDF.ItemClick
-        Try
-            If dlg_OpenEaadhar.ShowDialog = DialogResult.OK Then
-                Using Processor As New PdfDocumentProcessor
-                    AddHandler Processor.PasswordRequested, AddressOf view_PDF_PasswordRequested
-                    Processor.LoadDocument(dlg_OpenEaadhar.FileName)
-                    Processor.RemoveFormField("Signature1")
-
-                    Dim encryptionOptions As PdfEncryptionOptions = New PdfEncryptionOptions()
-                    encryptionOptions.PrintingPermissions = PdfDocumentPrintingPermissions.Allowed
-                    encryptionOptions.DataExtractionPermissions = PdfDocumentDataExtractionPermissions.Allowed
-                    encryptionOptions.ModificationPermissions = PdfDocumentModificationPermissions.Allowed
-                    encryptionOptions.InteractivityPermissions = PdfDocumentInteractivityPermissions.Allowed
-                    encryptionOptions.OwnerPasswordString = ""
-                    encryptionOptions.UserPasswordString = ""
-
-                    Using MS As New IO.MemoryStream
-                        Processor.SaveDocument(MS, New PdfSaveOptions With {.EncryptionOptions = encryptionOptions})
-                        If view_PDF.IsDocumentOpened Then view_PDF.CloseDocument()
-                        view_PDF.LoadDocument(MS)
-                    End Using
-                End Using
-            End If
-        Catch ex As Exception
-            MsgBox(ex.Message, MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "Error")
-        End Try
+        If dlg_OpenEaadhar.ShowDialog = DialogResult.OK Then
+            OpenFile(dlg_OpenEaadhar.FileName)
+        End If
     End Sub
 
     Private Sub view_PDF_PasswordRequested(sender As Object, e As PdfPasswordRequestedEventArgs) Handles view_PDF.PasswordRequested
@@ -65,6 +43,45 @@ Public Class frm_Main
             Dim dlg As New frm_ReportViewer(New BigCard(New BigCardItem(Front, Back)))
             dlg.Show()
         End If
+    End Sub
+#End Region
+
+#Region "Form Events"
+    Private Sub frm_Main_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        If My.Application.CommandLineArgs.Count > 0 Then
+            Dim Filename As String = My.Application.CommandLineArgs(0)
+            If My.Computer.FileSystem.FileExists(Filename) Then
+                OpenFile(Filename)
+            End If
+        End If
+    End Sub
+#End Region
+
+#Region "Subs"
+    Sub OpenFile(ByVal Filename As String)
+        Try
+            Using Processor As New PdfDocumentProcessor
+                AddHandler Processor.PasswordRequested, AddressOf view_PDF_PasswordRequested
+                Processor.LoadDocument(Filename)
+                Processor.RemoveFormField("Signature1")
+
+                Dim encryptionOptions As PdfEncryptionOptions = New PdfEncryptionOptions()
+                encryptionOptions.PrintingPermissions = PdfDocumentPrintingPermissions.Allowed
+                encryptionOptions.DataExtractionPermissions = PdfDocumentDataExtractionPermissions.Allowed
+                encryptionOptions.ModificationPermissions = PdfDocumentModificationPermissions.Allowed
+                encryptionOptions.InteractivityPermissions = PdfDocumentInteractivityPermissions.Allowed
+                encryptionOptions.OwnerPasswordString = ""
+                encryptionOptions.UserPasswordString = ""
+
+                Using MS As New IO.MemoryStream
+                    Processor.SaveDocument(MS, New PdfSaveOptions With {.EncryptionOptions = encryptionOptions})
+                    If view_PDF.IsDocumentOpened Then view_PDF.CloseDocument()
+                    view_PDF.LoadDocument(MS)
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical + MsgBoxStyle.OkOnly, "Error")
+        End Try
     End Sub
 #End Region
 
