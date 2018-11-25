695d961dc1e485a055e1d83877ba0ccc649c500b
Devil7DK <dineshthangavel47@gmail.com>
2018-11-25 20:53:36 +0530

    Improve UI: Remove Unwanted Controls

--- Devil7 - Aadhar Card Printing Software/frm_Main.Designer.vb
+++ Devil7 - Aadhar Card Printing Software/frm_Main.Designer.vb
@@ -20,21 +20,21 @@ Partial Class frm_Main
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_Main))
+        Dim SuperToolTip1 As DevExpress.Utils.SuperToolTip = New DevExpress.Utils.SuperToolTip()
+        Dim ToolTipTitleItem1 As DevExpress.Utils.ToolTipTitleItem = New DevExpress.Utils.ToolTipTitleItem()
+        Dim ToolTipItem1 As DevExpress.Utils.ToolTipItem = New DevExpress.Utils.ToolTipItem()
+        Dim SuperToolTip2 As DevExpress.Utils.SuperToolTip = New DevExpress.Utils.SuperToolTip()
+        Dim ToolTipTitleItem2 As DevExpress.Utils.ToolTipTitleItem = New DevExpress.Utils.ToolTipTitleItem()
+        Dim ToolTipItem2 As DevExpress.Utils.ToolTipItem = New DevExpress.Utils.ToolTipItem()
+        Dim SuperToolTip3 As DevExpress.Utils.SuperToolTip = New DevExpress.Utils.SuperToolTip()
+        Dim ToolTipTitleItem3 As DevExpress.Utils.ToolTipTitleItem = New DevExpress.Utils.ToolTipTitleItem()
+        Dim ToolTipItem3 As DevExpress.Utils.ToolTipItem = New DevExpress.Utils.ToolTipItem()
         Dim SuperToolTip4 As DevExpress.Utils.SuperToolTip = New DevExpress.Utils.SuperToolTip()
         Dim ToolTipTitleItem4 As DevExpress.Utils.ToolTipTitleItem = New DevExpress.Utils.ToolTipTitleItem()
         Dim ToolTipItem4 As DevExpress.Utils.ToolTipItem = New DevExpress.Utils.ToolTipItem()
         Dim SuperToolTip5 As DevExpress.Utils.SuperToolTip = New DevExpress.Utils.SuperToolTip()
         Dim ToolTipTitleItem5 As DevExpress.Utils.ToolTipTitleItem = New DevExpress.Utils.ToolTipTitleItem()
         Dim ToolTipItem5 As DevExpress.Utils.ToolTipItem = New DevExpress.Utils.ToolTipItem()
-        Dim SuperToolTip6 As DevExpress.Utils.SuperToolTip = New DevExpress.Utils.SuperToolTip()
-        Dim ToolTipTitleItem6 As DevExpress.Utils.ToolTipTitleItem = New DevExpress.Utils.ToolTipTitleItem()
-        Dim ToolTipItem6 As DevExpress.Utils.ToolTipItem = New DevExpress.Utils.ToolTipItem()
-        Dim SuperToolTip7 As DevExpress.Utils.SuperToolTip = New DevExpress.Utils.SuperToolTip()
-        Dim ToolTipTitleItem7 As DevExpress.Utils.ToolTipTitleItem = New DevExpress.Utils.ToolTipTitleItem()
-        Dim ToolTipItem7 As DevExpress.Utils.ToolTipItem = New DevExpress.Utils.ToolTipItem()
-        Dim SuperToolTip8 As DevExpress.Utils.SuperToolTip = New DevExpress.Utils.SuperToolTip()
-        Dim ToolTipTitleItem8 As DevExpress.Utils.ToolTipTitleItem = New DevExpress.Utils.ToolTipTitleItem()
-        Dim ToolTipItem8 As DevExpress.Utils.ToolTipItem = New DevExpress.Utils.ToolTipItem()
         Me.RibbonControl = New DevExpress.XtraBars.Ribbon.RibbonControl()
         Me.btn_OpenPDF = New DevExpress.XtraBars.BarButtonItem()
         Me.btn_Print_Full = New DevExpress.XtraBars.BarButtonItem()
@@ -46,13 +46,9 @@ Partial Class frm_Main
         Me.rpg_Print = New DevExpress.XtraBars.Ribbon.RibbonPageGroup()
         Me.RibbonStatusBar = New DevExpress.XtraBars.Ribbon.RibbonStatusBar()
         Me.view_PDF = New DevExpress.XtraPdfViewer.PdfViewer()
-        Me.SplitContainerControl1 = New DevExpress.XtraEditors.SplitContainerControl()
-        Me.view_Print = New System.Windows.Forms.PrintPreviewControl()
         Me.dlg_OpenEaadhar = New System.Windows.Forms.OpenFileDialog()
         Me.dlg_SaveEaadhar = New System.Windows.Forms.SaveFileDialog()
         CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.SplitContainerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SplitContainerControl1.SuspendLayout()
         Me.SuspendLayout()
         '
         'RibbonControl
@@ -74,12 +70,12 @@ Partial Class frm_Main
         Me.btn_OpenPDF.Id = 1
         Me.btn_OpenPDF.ImageOptions.SvgImage = CType(resources.GetObject("btn_OpenPDF.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
         Me.btn_OpenPDF.Name = "btn_OpenPDF"
-        ToolTipTitleItem4.Text = "Open"
-        ToolTipItem4.LeftIndent = 6
-        ToolTipItem4.Text = "Open EAadhar PDF Downloaded from UIDAI Website"
-        SuperToolTip4.Items.Add(ToolTipTitleItem4)
-        SuperToolTip4.Items.Add(ToolTipItem4)
-        Me.btn_OpenPDF.SuperTip = SuperToolTip4
+        ToolTipTitleItem1.Text = "Open"
+        ToolTipItem1.LeftIndent = 6
+        ToolTipItem1.Text = "Open EAadhar PDF Downloaded from UIDAI Website"
+        SuperToolTip1.Items.Add(ToolTipTitleItem1)
+        SuperToolTip1.Items.Add(ToolTipItem1)
+        Me.btn_OpenPDF.SuperTip = SuperToolTip1
         '
         'btn_Print_Full
         '
@@ -87,12 +83,12 @@ Partial Class frm_Main
         Me.btn_Print_Full.Id = 2
         Me.btn_Print_Full.ImageOptions.SvgImage = CType(resources.GetObject("btn_Print_Full.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
         Me.btn_Print_Full.Name = "btn_Print_Full"
-        ToolTipTitleItem5.Text = "Full Size Print"
-        ToolTipItem5.LeftIndent = 6
-        ToolTipItem5.Text = "Print Full Page Aadhar In the Downloaded Format"
-        SuperToolTip5.Items.Add(ToolTipTitleItem5)
-        SuperToolTip5.Items.Add(ToolTipItem5)
-        Me.btn_Print_Full.SuperTip = SuperToolTip5
+        ToolTipTitleItem2.Text = "Full Size Print"
+        ToolTipItem2.LeftIndent = 6
+        ToolTipItem2.Text = "Print Full Page Aadhar In the Downloaded Format"
+        SuperToolTip2.Items.Add(ToolTipTitleItem2)
+        SuperToolTip2.Items.Add(ToolTipItem2)
+        Me.btn_Print_Full.SuperTip = SuperToolTip2
         '
         'btn_Print_Card
         '
@@ -100,13 +96,13 @@ Partial Class frm_Main
         Me.btn_Print_Card.Id = 3
         Me.btn_Print_Card.ImageOptions.SvgImage = CType(resources.GetObject("btn_Print_Card.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
         Me.btn_Print_Card.Name = "btn_Print_Card"
-        ToolTipTitleItem6.Text = "Small Card (1 Sided)"
-        ToolTipItem6.LeftIndent = 6
-        ToolTipItem6.Text = "Print in Small ID Card Size. Both Front & Back Sides of Aadhar will Be Printed in" &
+        ToolTipTitleItem3.Text = "Small Card (1 Sided)"
+        ToolTipItem3.LeftIndent = 6
+        ToolTipItem3.Text = "Print in Small ID Card Size. Both Front & Back Sides of Aadhar will Be Printed in" &
     " Same Page."
-        SuperToolTip6.Items.Add(ToolTipTitleItem6)
-        SuperToolTip6.Items.Add(ToolTipItem6)
-        Me.btn_Print_Card.SuperTip = SuperToolTip6
+        SuperToolTip3.Items.Add(ToolTipTitleItem3)
+        SuperToolTip3.Items.Add(ToolTipItem3)
+        Me.btn_Print_Card.SuperTip = SuperToolTip3
         '
         'btn_Print_FullCard
         '
@@ -114,14 +110,14 @@ Partial Class frm_Main
         Me.btn_Print_FullCard.Id = 4
         Me.btn_Print_FullCard.ImageOptions.SvgImage = CType(resources.GetObject("btn_Print_FullCard.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
         Me.btn_Print_FullCard.Name = "btn_Print_FullCard"
-        ToolTipTitleItem7.Text = "Full Card Print (2 Sided)"
-        ToolTipItem7.LeftIndent = 6
-        ToolTipItem7.Text = "Print Full Size Aadhar in Front & Back Format." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Note: You must set the followin" &
+        ToolTipTitleItem4.Text = "Full Card Print (2 Sided)"
+        ToolTipItem4.LeftIndent = 6
+        ToolTipItem4.Text = "Print Full Size Aadhar in Front & Back Format." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Note: You must set the followin" &
     "g Custom Paper Size in your Printer Preferences & Enable two sided printing." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) &
     "SIZE : 3.5"" x 8.25"""
-        SuperToolTip7.Items.Add(ToolTipTitleItem7)
-        SuperToolTip7.Items.Add(ToolTipItem7)
-        Me.btn_Print_FullCard.SuperTip = SuperToolTip7
+        SuperToolTip4.Items.Add(ToolTipTitleItem4)
+        SuperToolTip4.Items.Add(ToolTipItem4)
+        Me.btn_Print_FullCard.SuperTip = SuperToolTip4
         '
         'btn_Save
         '
@@ -130,12 +126,12 @@ Partial Class frm_Main
         Me.btn_Save.Id = 5
         Me.btn_Save.ImageOptions.SvgImage = CType(resources.GetObject("btn_Save.ImageOptions.SvgImage"), DevExpress.Utils.Svg.SvgImage)
         Me.btn_Save.Name = "btn_Save"
-        ToolTipTitleItem8.Text = "Save"
-        ToolTipItem8.LeftIndent = 6
-        ToolTipItem8.Text = "Save EAadhar PDF Without Encryption, Digital Sign Image  & Downloaded Date Time"
-        SuperToolTip8.Items.Add(ToolTipTitleItem8)
-        SuperToolTip8.Items.Add(ToolTipItem8)
-        Me.btn_Save.SuperTip = SuperToolTip8
+        ToolTipTitleItem5.Text = "Save"
+        ToolTipItem5.LeftIndent = 6
+        ToolTipItem5.Text = "Save EAadhar PDF Without Encryption, Digital Sign Image  & Downloaded Date Time"
+        SuperToolTip5.Items.Add(ToolTipTitleItem5)
+        SuperToolTip5.Items.Add(ToolTipItem5)
+        Me.btn_Save.SuperTip = SuperToolTip5
         '
         'rp_Home
         '
@@ -169,33 +165,10 @@ Partial Class frm_Main
         'view_PDF
         '
         Me.view_PDF.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.view_PDF.Location = New System.Drawing.Point(0, 0)
+        Me.view_PDF.Location = New System.Drawing.Point(0, 143)
         Me.view_PDF.Name = "view_PDF"
-        Me.view_PDF.Size = New System.Drawing.Size(394, 275)
+        Me.view_PDF.Size = New System.Drawing.Size(667, 275)
         Me.view_PDF.TabIndex = 2
-        '
-        'SplitContainerControl1
-        '
-        Me.SplitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.SplitContainerControl1.FixedPanel = DevExpress.XtraEditors.SplitFixedPanel.Panel2
-        Me.SplitContainerControl1.Location = New System.Drawing.Point(0, 143)
-        Me.SplitContainerControl1.Name = "SplitContainerControl1"
-        Me.SplitContainerControl1.Panel1.Controls.Add(Me.view_PDF)
-        Me.SplitContainerControl1.Panel1.Text = "Panel1"
-        Me.SplitContainerControl1.Panel2.Controls.Add(Me.view_Print)
-        Me.SplitContainerControl1.Panel2.Text = "Panel2"
-        Me.SplitContainerControl1.Size = New System.Drawing.Size(667, 275)
-        Me.SplitContainerControl1.SplitterPosition = 268
-        Me.SplitContainerControl1.TabIndex = 5
-        Me.SplitContainerControl1.Text = "SplitContainerControl1"
-        '
-        'view_Print
-        '
-        Me.view_Print.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.view_Print.Location = New System.Drawing.Point(0, 0)
-        Me.view_Print.Name = "view_Print"
-        Me.view_Print.Size = New System.Drawing.Size(268, 275)
-        Me.view_Print.TabIndex = 0
         '
         'dlg_OpenEaadhar
         '
@@ -213,7 +186,7 @@ Partial Class frm_Main
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(667, 449)
-        Me.Controls.Add(Me.SplitContainerControl1)
+        Me.Controls.Add(Me.view_PDF)
         Me.Controls.Add(Me.RibbonStatusBar)
         Me.Controls.Add(Me.RibbonControl)
         Me.Name = "frm_Main"
@@ -221,8 +194,6 @@ Partial Class frm_Main
         Me.StatusBar = Me.RibbonStatusBar
         Me.Text = "frm_Main"
         CType(Me.RibbonControl, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.SplitContainerControl1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.SplitContainerControl1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -233,8 +204,6 @@ Partial Class frm_Main
     Friend WithEvents rpg_PDF As DevExpress.XtraBars.Ribbon.RibbonPageGroup
     Friend WithEvents RibbonStatusBar As DevExpress.XtraBars.Ribbon.RibbonStatusBar
     Friend WithEvents view_PDF As DevExpress.XtraPdfViewer.PdfViewer
-    Friend WithEvents SplitContainerControl1 As DevExpress.XtraEditors.SplitContainerControl
-    Friend WithEvents view_Print As PrintPreviewControl
     Friend WithEvents btn_OpenPDF As DevExpress.XtraBars.BarButtonItem
     Friend WithEvents dlg_OpenEaadhar As OpenFileDialog
     Friend WithEvents btn_Print_Full As DevExpress.XtraBars.BarButtonItem
