eb20518123c471ad00b9bd9933b50be993dbc6e4
Devil7DK <dineshthangavel47@gmail.com>
2018-11-25 15:35:15 +0530

    Implement Opening PDF

--- Devil7 - Aadhar Card Printing Software/frm_Main.vb
+++ Devil7 - Aadhar Card Printing Software/frm_Main.vb
@@ -1,3 +1,11 @@
 ﻿Public Class frm_Main
 
+#Region "Button Events"
+    Private Sub btn_OpenPDF_ItemClick(sender As Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles btn_OpenPDF.ItemClick
+        If dlg_OpenEaadhar.ShowDialog = DialogResult.OK Then
+            view_PDF.LoadDocument(dlg_OpenEaadhar.FileName)
+        End If
+    End Sub
+#End Region
+
 End Class
